56046bbca33e040e006808be551657ae13d8c9d8
vsilver_eagle <92125893+vsilver-eagle@users.noreply.github.com>
2022-04-16 00:18:39 +0200

    Ensure that Word parameter is not empty

--- DictionaryApiNet/DictionaryApi.cs
+++ DictionaryApiNet/DictionaryApi.cs
@@ -12,11 +12,21 @@ namespace DictionaryApiNet
 
         public static WordDefinition GetWordDefinition(string Word)
         {
+            if (string.IsNullOrEmpty(Word))
+            {
+                throw new ArgumentException($"'{nameof(Word)}' cannot be null or empty.", nameof(Word));
+            }
+
             return GetWordDefinitionAsync(Word).Result;
         }
 
         public static async Task<WordDefinition> GetWordDefinitionAsync(string Word)
         {
+            if (string.IsNullOrEmpty(Word))
+            {
+                throw new ArgumentException($"'{nameof(Word)}' cannot be null or empty.", nameof(Word));
+            }
+
             using var Client = new HttpClient();
             var Data = await Client.GetStringAsync(GetRequestAddress(Word));
             
