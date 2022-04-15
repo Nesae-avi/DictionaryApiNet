b4f53c5a3a7edab535b8797dce37c88240a8cb7b
vsilver_eagle <92125893+vsilver-eagle@users.noreply.github.com>
2022-04-15 23:12:34 +0200

    Update example

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,6 +1,6 @@
 ﻿using DictionaryApiNet;
 
-var WordData = DictionaryApi.GetWordDefinition("Cat");
+var WordData = await DictionaryApi.GetWordDefinitionAsync("greeting");
 
 Console.WriteLine($"{WordData.Word} [{WordData.Phonetics[0].Text}] - {WordData.Meanings[0].Definitions[0].DefinitionText}");
 Console.ReadKey();
