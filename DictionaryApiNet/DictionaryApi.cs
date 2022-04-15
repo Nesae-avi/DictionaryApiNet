31bcdc53fa2003e23be722f7e0e9704a66ee2ffc
vsilver_eagle <92125893+vsilver-eagle@users.noreply.github.com>
2022-04-16 00:18:05 +0200

    Add synchronous call

--- DictionaryApiNet/DictionaryApi.cs
+++ DictionaryApiNet/DictionaryApi.cs
@@ -10,10 +10,15 @@ namespace DictionaryApiNet
             return $"https://api.dictionaryapi.dev/api/v2/entries/en/{RequestedWord}";
         }
 
-        public static async Task<WordDefinition> GetWordDefinitionAsync(string RequestedWord)
+        public static WordDefinition GetWordDefinition(string Word)
+        {
+            return GetWordDefinitionAsync(Word).Result;
+        }
+
+        public static async Task<WordDefinition> GetWordDefinitionAsync(string Word)
         {
             using var Client = new HttpClient();
-            var Data = await Client.GetStringAsync(GetRequestAddress(RequestedWord));
+            var Data = await Client.GetStringAsync(GetRequestAddress(Word));
             
             return WordDefinition.FromJson(Data);
         }
